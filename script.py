9a2ad4b3fe5ab29f710a335dc84bbccc59c96cf6
Twan47 <anthonynewell171@gmail.com>
2023-02-28 12:21:20 -0500

    Completed! although I think the +5 for visual arts is wrong.

--- script.py
+++ script.py
@@ -3,3 +3,10 @@ subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
 gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
 print(gradebook)
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+gradebook[5][1] + 5
+gradebook.remove(["poetry", 85])
+gradebook.append(["poetry", "Pass"])
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)

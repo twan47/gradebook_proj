dd7e3b6f8ff3040602d90c797e04e30bb1cf86ab
Twan47 <anthonynewell171@gmail.com>
2023-02-28 11:48:29 -0500

    added combined lists within gradebook. and printed

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
 last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
 subjects = ["physics", "calculus", "poetry", "history"]
-grades = [[98], [97], [85], [88]]
+grades = [98, 97, 85, 88]
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+print(gradebook)
